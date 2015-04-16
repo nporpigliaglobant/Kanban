88accc35bd0d48d49b66f8a19200bebc0bc5d69f
Marcelo Dessal <marcelodessal@Marcelos-MacBook-Pro.local>
2015-04-16 11:50:56 -0300

    Added basic navigation.

---
+++ Kanban/Login.h
@@ -0,0 +1,13 @@
+//
+//  Login.h
+//  Kanban
+//
+//  Created by Marcelo Dessal on 4/16/15.
+//  Copyright (c) 2015 Globant. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Login : UIViewController
+
+@end
---
+++ Kanban/Login.m
@@ -0,0 +1,13 @@
+//
+//  Login.m
+//  Kanban
+//
+//  Created by Marcelo Dessal on 4/16/15.
+//  Copyright (c) 2015 Globant. All rights reserved.
+//
+
+#import "Login.h"
+
+@implementation Login
+
+@end
